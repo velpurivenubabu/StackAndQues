21065863c017611a13d19d6a75d5fa4754ac0abc
venubabu <velpurivenubabu123@gmail.com>
2023-05-11 17:38:53 +0530

    [Venubabu] Done with WelcomeMessage

---
+++ StackAndQues/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StackAndQues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome To DataStructures(Stack And Ques!)");
+        }
+    }
+}
